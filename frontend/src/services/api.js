e22b528e5c19329aa54f4d2c1e72eed75667e547
blackcodd <u2104046@student.cuet.ac.bd>
2026-08-16 10:32:25 +0600

    Update for deploy variable port

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,5 @@
-const API_URL = 'http://localhost:5080';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5080';
+
 
 export async function fetchTasks() {
   const response = await fetch(`${API_URL}/api/tasks`);
